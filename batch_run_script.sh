498323db6fa30a3c1aa95d961b5287cfadd6c0ab
chenziao <zc963@engr-nelvm-res.engineering.missouri.edu>
2025-10-23 00:10:27 -0500

    Correct requirement package error; add batch script for running on server

---
+++ batch_run_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -J analyze_allen_visual_coding_script
+#SBATCH -o ./stdout/run_script%j.out
+#SBATCH -e ./stdout/run_script%j.error
+#SBATCH -t 0-12:00:00
+
+#SBATCH -N 1
+#SBATCH -n 1
+##SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=16G
+
+
+START=$(date)
+
+unset DISPLAY
+python scripts/find_probe_channels.py --session_set all
+
+END=$(date)
+
+echo "Started running at $START."
